6a3fdd9f03549833572c6abf032e01eb731f1da7
Androix777 <androix77@gmail.com>
2022-07-09 15:43:01 +0500

    Good mask?

--- src/front/tables/test-table.js
+++ src/front/tables/test-table.js
@@ -112,10 +112,14 @@ var dateEditor = function(cell, onRendered, success, cancel){
     return input;
 };
 
-//Create Date Editor
-var durationEditor = function(cell, onRendered, success, cancel){
-    var cellValue = convertToDuration(cell.getValue());
-    var input = document.createElement("input");
+
+var durationEditor = function(cell, onRendered, success, cancel)
+{
+    let cellValue = convertToDuration(cell.getValue());
+    let input = document.createElement("input");
+
+    let currentValueIndex = 0
+    let isChanging = false
 
     input.style.padding = "4px";
     input.style.width = "100%";
@@ -128,50 +132,231 @@ var durationEditor = function(cell, onRendered, success, cancel){
         input.style.height = "100%";
     });
 
-    function onChange(){
-        if(input.value != cellValue){
-            console.log(input.value);
-            success(convertFromDuration(input.value));
-        }else{
-            cancel();
-        }
-    }
-    let originalString = input.value
-
-    input.addEventListener("input", function(event)
+    input.addEventListener("beforeinput", (event) =>
     {
-        
-        console.log(event);
-        console.log(originalString + " -> " + event.target.value);
-        let changes = getChanges(event, originalString)
-        event.target.value = fixDurationString(changes, originalString, event.target.value)
-
-        originalString = event.target.value
-        event.target.selectionStart = changes.position
-        event.target.selectionEnd = changes.position
-    });
-
-    input.addEventListener("select", (event) =>
-    {
-        console.log(event)
+        event.preventDefault();
+        tryChangeValue(event.data, event.inputType)
+        selectValueIndex(currentValueIndex)
     })
 
-    //submit new value on blur or change
+    input.addEventListener("focus", (event) =>
+    {
+        event.preventDefault();
+        isChanging = false
+        selectValueIndex(0)
+    })
+
+    input.addEventListener("mousedown", (event) =>
+    {
+        event.preventDefault();
+        isChanging = false
+        selectValueIndex(valueIndexByPosition(mousePositionInChars(event.layerX)))
+    })
+
     input.addEventListener("blur", onChange);
 
-    //submit new value on enter
-    input.addEventListener("keydown", function(e){
-        if(e.keyCode == 13){
+    input.addEventListener("keydown", (event) =>
+    {
+        if(event.key == 'ArrowLeft')
+        {
+            event.preventDefault();
+            isChanging = false
+            selectValueIndex(currentValueIndex - 1)
+        }
+
+        if(event.key == 'ArrowRight')
+        {
+            event.preventDefault();
+            isChanging = false
+            selectValueIndex(currentValueIndex + 1)
+        }
+
+        if(event.key == 'Enter')
+        {
             onChange();
         }
 
-        if(e.keyCode == 27){
+        if(event.key == 'Esc')
+        {
             cancel();
         }
     });
 
-    return input;
-};
+    function onChange()
+    {
+        if(input.value != cellValue)
+        {
+            success(convertFromDuration(input.value));
+        }
+        else
+        {
+            cancel();
+        }
+    }
+
+    function selectValueIndex(index)
+    {
+        let positions = getValuePositions()
+
+        if(index < 0 || index > 2) return
+
+        input.setSelectionRange(positions[index][0], positions[index][1])
+
+        currentValueIndex = index
+    }
+
+    function getValuePositions()
+    {
+        var indexes = []
+        var start = 0
+        var end = 0
+        var str = input.value
+
+        var positions = []
+
+        for(var i=0; i<str.length; i++) 
+        {
+            if (str[i] === ":") indexes.push(i);
+        }
+        
+        if(indexes.length != 2)
+        {
+            return
+        }
+
+        for(var index = 0; index < 3; index++)
+        {
+            if(index == 0)
+            {
+                start = 0
+            }
+            else
+            {
+                start = indexes[index - 1] + 1
+            }
+
+            if(index == 2)
+            {
+                end = str.length
+            }
+            else
+            {
+                end = indexes[index]
+            }
+            positions.push([start, end])
+        }
+        
+        return positions
+    }
+
+    function mousePositionInChars(layerX) 
+    {
+        var element_base_browser_styles = window.getComputedStyle(input);
+
+        var add_char_pixal_lengths = 0
+
+        var myStringArray = input.value.split("")
+        var arrayLength = myStringArray.length;
+        for (var i = 0; i <= arrayLength; i++) 
+        {
+            var get_char_value = getTextWidth(
+                myStringArray[i],
+                element_base_browser_styles.fontFamily +
+                " " +
+                element_base_browser_styles.fontSize
+            );
+
+            add_char_pixal_lengths =
+            add_char_pixal_lengths + get_char_value + 1.311111111111;
+
+            if (add_char_pixal_lengths > layerX) 
+            {
+                return i
+            }
+        }
+        return 0
+
+        function getTextWidth(text, font) 
+        {
+            // TODO: закэшировать канвас
+            var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"))
+            var context = canvas.getContext("2d")
+            context.font = font
+            var metrics = context.measureText(text)
+            return metrics.width
+        }
+    }
+
+    function valueIndexByPosition(position)
+    {
+        var valuesPositions = getValuePositions()
+        for(let i = 0; i < 3; i++)
+        {
+            if(position <= valuesPositions[i][1]) return i
+        }
+        
+    }
+
+    function getCurrentValue()
+    {
+        let positions = getValuePositions()
+        return parseInt(input.value.slice(positions[currentValueIndex][0], positions[currentValueIndex][1]))
+    }
+
+
+    function tryChangeValue(newValue, inputType)
+    {
+        var maxValues = [23, 59, 59]
+        var newValue = parseInt(newValue)
+        var currentValue = getCurrentValue()
+
+        if(inputType != "insertText") return
+        if(newValue == "undefined") return
+        if(newValue < 0 || newValue > 9) return
+        
+        if(isChanging)
+        {
+            let addedValue = currentValue * 10 + newValue
+            if(addedValue > maxValues[currentValueIndex])
+            {
+                isChanging = false
+                selectValueIndex(currentValueIndex + 1)
+                tryChangeValue(newValue, inputType)
+            }
+            else
+            {
+                setValue(currentValueIndex, addedValue)
+                if (addedValue * 10 > maxValues[currentValueIndex])
+                {
+                    isChanging = false
+                    selectValueIndex(currentValueIndex + 1)
+                }
+            }
+        }
+        else
+        {
+            // добавить проверку на меньше 9
+            setValue(currentValueIndex, newValue)
+            if (newValue * 10 > maxValues[currentValueIndex])
+            {
+                isChanging = false
+                selectValueIndex(currentValueIndex + 1)
+            }
+            else
+            {
+                isChanging = true
+            }
+        }
+    }
+
+    function setValue(index, value)
+    {
+        let positions = getValuePositions()
+        input.value = input.value.slice(0, positions[index][0]) + value + input.value.slice(positions[index][1])
+    }
+
+    return input
+}
 
 //custom max min header filter
 var minMaxFilterEditor = function(cell, onRendered, success, cancel, editorParams){
@@ -269,41 +454,4 @@ function convertFromDuration(value)
 {
     var duration = value.match(/(\d+):(\d+):(\d+)/);
     return (Duration.fromObject({hours:duration[1], minutes:duration[2], seconds:duration[3]}).toMillis() / 1000)
-}
-
-function getChanges(event, originalString)
-{
-    let position
-    let deletedString
-    let isValid = true
-
-    if(["deleteContentBackward", "deleteContentForward", "deleteByCut"].includes(event.inputType))
-    {
-        position = event.target.selectionStart
-        let lenDiff = originalString.length - event.target.value.length
-        deletedString = originalString.slice(position, position + lenDiff)
-    }
-    else
-    {
-        isValid = false
-    }
-
-    return {
-        position: position,
-        deletedString: deletedString,
-        isValid: isValid
-    }
-}
-
-function fixDurationString(durationChanges, originalString, newString)
-{
-    if(!durationChanges.isValid)
-    {
-        return originalString
-    }
-    let fixedDeleted = durationChanges.deletedString.replace(/[^:*]/g, '');
-    console.log(fixedDeleted)
-    let fixedString = [newString.slice(0, durationChanges.position), fixedDeleted, newString.slice(durationChanges.position)].join('')
-    return fixedString
-
 }
