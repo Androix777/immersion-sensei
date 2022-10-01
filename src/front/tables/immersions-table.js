7b570e7f36214e5ba07f519893ad8d370f884df9
Airplane <letadllo@gmail.com>
2022-10-01 10:59:17 +0500

    Tag string editor

--- src/front/tables/immersions-table.js
+++ src/front/tables/immersions-table.js
@@ -119,6 +119,89 @@ export function createImmersionsTable(immersionsData, worksData, tagsData, divID
             {
                 title:"Tags", 
                 field:"tags",
+                editor: function(cell, onRendered, success, cancel)
+                {
+                    var tagsDataReversed = Object.fromEntries(Object.entries(tagsData).map(([k, v]) => [v, k]));
+                    var cellValue = numsToTags(cell.getValue());
+                    var input = document.createElement("input");
+
+                    input.value = cellValue;
+
+                    onRendered(function(){
+                        input.focus();
+                        input.style.height = "100%";
+                    });
+
+                    function onChange()
+                    {
+                        if(input.value != cellValue)
+                        {
+                            success(tagsToNums(input.value));
+                        }
+                        else
+                        {
+                            cancel();
+                        }
+                    }
+
+                    function numsToTags(numList)
+                    {
+                        console.log(numList);
+                        var tagString = '';
+
+                        numList.forEach(
+                            element =>
+                            {
+                                if(element in tagsData)
+                                {
+                                    tagString += tagsData[element] + ', ';
+                                }     
+                            }
+                        );
+
+                        if(tagString.slice(-2) == ', ')
+                        {
+                            tagString = tagString.slice(0, -2);
+                        }
+
+                        return tagString;
+                    }
+
+                    function tagsToNums(tagString)
+                    {
+                        var numList = [];
+                        var tagList = tagString.split(',').map(x => x.trim());
+                        
+                        tagList.forEach(
+                            element =>
+                            {
+                                if(element in tagsDataReversed)
+                                {
+                                    numList.push(tagsDataReversed[element]);
+                                }
+                            }
+                        );
+
+                        return numList;
+                    }
+
+                    input.addEventListener("blur", onChange);
+
+                    input.addEventListener("keydown", (event) =>
+                    {
+                        if(event.key == 'Enter')
+                        {
+                            onChange();
+                        }
+
+                        if(event.key == 'Esc')
+                        {
+                            cancel();
+                        }
+                    });
+
+                    return input;
+                },
                 formatter:(cell, formatterParams, onRendered) =>
                 {
                     var newValue = "";
@@ -129,12 +212,17 @@ export function createImmersionsTable(immersionsData, worksData, tagsData, divID
                         {
                             if(element in tagsData)
                             {
-                                newValue += tagsData[element] + " ";
-                            }
-                            
-                        });
+                                newValue += tagsData[element] + ", ";
+                            }                            
+                        }
+                    );
                     
-                        return newValue;
+                    if(newValue.slice(-2) == ', ')
+                    {
+                        newValue = newValue.slice(0, -2);
+                    }
+
+                    return newValue;
                 }
             }
         ],
