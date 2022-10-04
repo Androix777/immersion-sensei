1ed8beefd7f6ad277c44c96457e12c0cc4334d36
Airplane <letadllo@gmail.com>
2022-10-04 13:16:16 +0500

    TIme chart, chart formatting

--- src/front/charts/main-chart.js
+++ src/front/charts/main-chart.js
@@ -2,28 +2,42 @@ export function create(data, worksDataDict)
 {
     var chart = new dc.RowChart("#chart");
     var chart2 = new dc.BarChart("#chart2");
+    var chart3 = new dc.BarChart('#chart3');
+
+    var margins = {top: 10, bottom: 50, left: 50, right: 0};
 
     var ndx = crossfilter(data);
     var workDimension = ndx.dimension((d) => { return d.work_id; });
     var dateDimension = ndx.dimension((d) => { return Date.parse(d.date); });
-    var countGroup = workDimension.group();
+    var countGroup = workDimension.group().reduceSum(function (d) {return d.characters;});
     var charactersSumGroup = dateDimension.group().reduceSum(function(d) {return d.characters;});
+    var timeSumGroup = dateDimension.group().reduceSum(function (d) {return d.time});
 
     
     chart
-        .width(768)
+        .width(null)
         .height(480)
+        .margins(margins)
         .elasticX(true)
         .dimension(workDimension)
         .group(countGroup)
         .label(p => worksDataDict[p.key]);
 
     chart2
-        .width(768)
+        .width(null)
         .height(480)
+        .margins(margins)
         .x(d3.scaleTime().domain([new Date(2021, 8, 1), new Date(2022, 9, 28)]))
         .dimension(dateDimension)
         .group(charactersSumGroup);
+    
+    chart3
+        .width(null)
+        .height(480)
+        .margins(margins)
+        .x(d3.scaleTime().domain([new Date(2021, 8, 1), new Date(2022, 9, 28)]))
+        .dimension(dateDimension)
+        .group(timeSumGroup);
     
     dc.renderAll();
 };
