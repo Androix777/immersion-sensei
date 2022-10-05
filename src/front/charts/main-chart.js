b6ec78ed15201a6e38692cc9a4d14b57ff71d82d
Airplane <letadllo@gmail.com>
2022-10-05 14:57:05 +0500

    Duration scale formatting?

--- src/front/charts/main-chart.js
+++ src/front/charts/main-chart.js
@@ -4,7 +4,7 @@ export function create(data, worksDataDict)
     var chart2 = new dc.BarChart("#chart2");
     var chart3 = new dc.BarChart('#chart3');
 
-    var margins = {top: 10, bottom: 50, left: 50, right: 0};
+    var margins = {top: 10, bottom: 50, left: 75, right: 0};
 
     var ndx = crossfilter(data);
     var workDimension = ndx.dimension((d) => { return d.work_id; });
@@ -30,7 +30,7 @@ export function create(data, worksDataDict)
         .x(d3.scaleTime().domain([new Date(2021, 8, 1), new Date(2022, 9, 28)]))
         .dimension(dateDimension)
         .group(charactersSumGroup);
-    
+     
     chart3
         .width(null)
         .height(480)
@@ -39,5 +39,13 @@ export function create(data, worksDataDict)
         .dimension(dateDimension)
         .group(timeSumGroup);
     
+    chart3.yAxis().tickFormat((d, i) => {return luxon.Duration.fromObject({seconds:d}).toFormat('h:mm:ss');});
+    var timeTickValues = [60 * 60];
+    while(Math.max.apply(Math, timeTickValues) < timeSumGroup.top(1)[0].value)
+    {
+        timeTickValues.push(Math.max.apply(Math, timeTickValues) + 60 * 60);
+    }
+    chart3.yAxis().tickValues(timeTickValues);
+
     dc.renderAll();
 };
