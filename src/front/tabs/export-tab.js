ccb2bce52d033cc9f2d2c66145b23a02858f459f
Airplane <letadllo@gmail.com>
2022-10-29 12:52:29 +0500

    Simple export preprocessing

--- src/front/tabs/export-tab.js
+++ src/front/tabs/export-tab.js
@@ -1,4 +1,5 @@
 import * as notifyOptions from '../notiflix/notify-options.js'
+import { Duration } from "../../../node_modules/luxon/build/es6/luxon.js";
 
 var toCleanUp = []
 
@@ -15,9 +16,11 @@ export async function show()
 
     exportButton.onclick = async () =>
     {
-        var data = await window.api.getImmersions();
+        collectExportData();
+        var data = await collectExportData();
         data = arrayToCSV(data, 'path');
         saveFile('export.csv', data, 'text/csv');
+        Notiflix.Notify.success('Export complete', notifyOptions.currentOptions);
     }   
 }
 
@@ -29,6 +32,52 @@ export function hide()
     {
         element.remove();
     })
+}
+
+async function collectExportData()
+{
+    var data = await window.api.getImmersions();
+    var worksData = await window.api.getWorks();
+    var tagsData = await window.api.getTags();
+    
+    var worksDataDict = {};
+    worksData.forEach(element =>
+    {
+        worksDataDict[element['id']] = element['title'];   
+    });
+
+    var tagsDataDict = {};
+    tagsData.forEach(element =>
+    {
+        tagsDataDict[element['id']] = element['name'];   
+    });
+
+    var dataKey = {'id' : 'id', 
+        'time' : 'time', 
+        'characters' : 'characters', 
+        'date' : 'date', 
+        'work_id' : 'work', 
+        'text_of_immersion_id' : 'text_of_immersion',
+        'tags' : 'tags'
+    };
+
+    data.forEach((immersion) =>
+    {
+        Object.keys(immersion).forEach((key) =>
+        {
+            if(dataKey[key] != key)
+            {
+                delete Object.assign(immersion, {[dataKey[key]]: immersion[key] })[key];
+            }
+        });
+        delete immersion[undefined];
+        immersion['work'] = worksDataDict[immersion['work']];
+        delete immersion['text_of_immersion'];
+        immersion['tags'] = immersion['tags'].map((tagID) => tagsDataDict[tagID]).join(' ');
+        immersion['time'] = Duration.fromObject({seconds:immersion['time']}).toFormat('hh:mm:ss');
+    });
+
+    return data;
 }
 
 function saveFile(name, content, type)
